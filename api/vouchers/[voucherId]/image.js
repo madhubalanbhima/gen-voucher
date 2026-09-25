9f6c4beb0e334f80de684258548464183a897aef
madhubalanbhima <madhubalan.bhima@gmail.com>
2026-09-25 15:02:20 +0530

    connect MongoDB before voucher image generation

--- api/vouchers/[voucherId]/image.js
+++ api/vouchers/[voucherId]/image.js
@@ -1,11 +1,19 @@
 const { voucherImage } = require("../../../server/src/controllers/voucherController");
+const connectDB = require("../../../server/src/db");
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   if (req.method !== "GET") {
     res.status(405).send("Method not allowed");
     return;
   }
 
+  try {
+    await connectDB();
+  } catch (err) {
+    res.status(503).send("Database is unavailable");
+    return;
+  }
+
   req.params = { voucherId: req.query.voucherId };
-  voucherImage(req, res);
+  return voucherImage(req, res);
 };
