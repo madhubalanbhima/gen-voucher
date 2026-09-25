00a549277b8d3444d2de40ae66533303d19b18dc
madhubalanbhima <madhubalan.bhima@gmail.com>
2026-09-25 14:14:00 +0530

    connect MongoDB in purchase function

--- api/vouchers/purchase.js
+++ api/vouchers/purchase.js
@@ -1,10 +1,18 @@
 const { purchaseVoucher } = require("../../server/src/controllers/voucherController");
+const connectDB = require("../../server/src/db");
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method not allowed" });
     return;
   }
 
-  purchaseVoucher(req, res);
+  try {
+    await connectDB();
+  } catch (err) {
+    res.status(503).json({ status: "error", message: "Database is unavailable." });
+    return;
+  }
+
+  return purchaseVoucher(req, res);
 };
