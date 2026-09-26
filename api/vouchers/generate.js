3d28e1e9c44332eb507ae10b7656ace5b0f019eb
madhubalanbhima <madhubalan.bhima@gmail.com>
2026-09-26 16:42:20 +0530

    Scheme voucher DB connection fixed

--- api/vouchers/generate.js
+++ api/vouchers/generate.js
@@ -1,10 +1,18 @@
 const { generateVoucher } = require("../../server/src/controllers/voucherController");
+const connectDB = require("../../server/src/db");
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method not allowed" });
     return;
   }
 
-  generateVoucher(req, res);
+  try {
+    await connectDB();
+  } catch (err) {
+    res.status(503).json({ status: "error", message: "Database is unavailable." });
+    return;
+  }
+
+  return generateVoucher(req, res);
 };
